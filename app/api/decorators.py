df784ed403faad9aea7ca4f5d6b5b53a92396ce7
maxlv7 <admin@maxlv.org>
2020-02-23 21:48:12 +0800

    更新auth_token校验

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -1,5 +1,9 @@
 from functools import wraps
+
+from flask import g
 from flask import request
+
+from app import jwt_util
 from app.utils.ret_util import response, RespStatus
 
 
@@ -19,7 +23,14 @@ def auth_token_required(f):
     def decorated_function(*args, **kwargs):
         jwt = request.headers.get("Authorization")
         if jwt is not None:
-            return f(*args, **kwargs)
+            # 检查auth_token的有效性
+            data = jwt_util.decode_auth_token(jwt)
+            if isinstance(data, RespStatus):
+                return response(*data.describe())
+            else:
+                # 存在g里面
+                g.auth_token = data
+                return f(*args, **kwargs)
         else:
             return response(*RespStatus.LackAuthorizationHeader.describe())
 
